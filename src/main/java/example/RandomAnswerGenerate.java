3f330f2b387c419f586bde1b61c96520c81f6c13
Dunka97 <1150311882@qq.com>
2020-07-23 22:35:49 +0800

    [KikiYang]feature:implement generate string is different

--- src/main/java/example/RandomAnswerGenerate.java
+++ src/main/java/example/RandomAnswerGenerate.java
@@ -9,6 +9,6 @@ package example;
 public class RandomAnswerGenerate implements AnswerGenerate {
     @Override
     public String generate() {
-        return "2234";
+        return "1234";
     }
 }
