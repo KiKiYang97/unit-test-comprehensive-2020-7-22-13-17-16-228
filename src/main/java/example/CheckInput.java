8da7c592374ffc80c9c4d994a1aa0aaa6792589e
Dunka97 <1150311882@qq.com>
2020-07-23 22:44:16 +0800

    [KikiYang]test:add should_false_when_input_12_given_check test

---
+++ src/main/java/example/CheckInput.java
@@ -0,0 +1,13 @@
+package example;
+
+/**
+ * @Author Dunka
+ * @Description //TODO
+ * @Date 22:43   2020/7/23
+ * @ClassName CheckInput
+ */
+public class CheckInput {
+    public boolean isValid(String s) {
+        return true;
+    }
+}
---
+++ src/test/java/example/CheckInputTest.java
@@ -0,0 +1,22 @@
+package example;
+
+import javafx.beans.binding.DoubleBinding;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @Author Dunka
+ * @Description //TODO
+ * @Date 22:36   2020/7/23
+ * @ClassName CheckInputTest
+ */
+public class CheckInputTest {
+    @Test
+    void should_false_when_input_12_given_check(){
+
+        CheckInput checkInput = new CheckInput();
+        boolean actual = checkInput.isValid("12");
+        assertEquals(false,actual);
+    }
+}
