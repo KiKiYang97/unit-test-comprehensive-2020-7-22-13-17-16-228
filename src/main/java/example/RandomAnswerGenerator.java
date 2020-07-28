ef3c84256a43109b0935b5ac57f9517f73cf86a0
Dunka97 <1150311882@qq.com>
2020-07-28 22:33:56 +0800

    [KikiYang]refactor : change 4 to ANSWER_LENGTH

--- src/main/java/example/RandomAnswerGenerator.java
+++ src/main/java/example/RandomAnswerGenerator.java
@@ -10,13 +10,16 @@ import java.util.Set;
  * @ClassName RandomAnswerGenerate
  */
 public class RandomAnswerGenerator implements AnswerGenerator {
+
+    public static final int ANSWER_LENGTH = 4;
+
     @Override
     public String generate() {
         StringBuilder answer = new StringBuilder();
         Set<Integer> set = new HashSet<>();
-        while (answer.length()<4){
+        while (answer.length() < ANSWER_LENGTH) {
             int number = (int) (Math.random() * 10);
-            if(!set.contains(number)){
+            if (!set.contains(number)) {
                 set.add(number);
                 answer.append(number);
             }
